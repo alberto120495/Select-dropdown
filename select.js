85859f942cbe21164f9928650c975c8df06c12cf
AlbertoWindows <albertopimentel04@gmail.com>
2021-01-25 19:57:26 -0600

    Inicio

---
+++ select.js
@@ -0,0 +1,13 @@
+export default class Select {
+  constructor(element) {
+    this.element = element;
+    this.customElement = document.createElement("div");
+    this.labelElement = document.createElement("span");
+    this.optionsCustomElement = document.createElement("ul");
+    setupCustomElement(this);
+  }
+}
+
+function setupCustomElement(select) {
+  select.customElement.classList.add("custom-select-container");
+}
